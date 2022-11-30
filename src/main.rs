cde13628547802d842ab118d9328c0e9e5ee85cf
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2022-11-30 09:27:06 +0100

    add the command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+pub(crate) struct CommandLineArguments {
+    /// Pass the glob pattern for file paths
+    #[clap(short = 'p', long)]
+    pub(crate) pattern: String,
+    
+    /// Pass the group(s) of codemods for execution
+    #[clap(short = 'g', long)]
+    pub(crate) group: Vec<String>,
+
+    /// Pass the limit for the number of files to inspect
+    #[clap(short = 'l', long)]
+    pub(crate) limit: Option<u64>,
+
+    /// Pass the limit for the number of files to inspect
+    #[clap(short = 'o', long)]
+    pub(crate) output_directory_path: Option<String>,
+}
+
 fn main() {
+    let command_line_arguments = CommandLineArguments::parse();
+
+    dbg!(command_line_arguments);
+
     println!("Hello, world!");
 }
