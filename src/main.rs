73dadb682d63809a740e9b3e75b0beaee462e5fe
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2022-12-05 16:31:10 +0100

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-use std::io::{BufReader, Read, Write};
+use std::io::{BufReader, Read};
 use std::path::Path;
 use std::thread;
 use std::{fs::File, path::PathBuf};
 
 use clap::Parser;
 use command_line_arguments::CommandLineArguments;
-use head_file::build_head_file_text;
 use json::object;
 use wax::{CandidatePath, Glob, Pattern};
 
@@ -17,8 +16,8 @@ mod paths;
 mod tree;
 mod queries;
 
-use crate::page_file::build_page_file_text;
-use crate::paths::{build_path_dto, get_pages_path_buf, build_page_document_path_buf_option};
+use crate::page_file::build_page_directory_messages;
+use crate::paths::{get_pages_path_buf, build_page_document_path_buf_option};
 use crate::queries::find_jsx_self_closing_element;
 use crate::tree::build_tree;
 
@@ -77,53 +76,11 @@ fn main() {
         let output_directory_path = command_line_arguments.output_directory_path.clone();
 
         thread::spawn(move || {
-            let path_dto_option = build_path_dto(&output_directory_path, &old_path_buf);
-
-            if path_dto_option.is_none() {
-                return;
-            }
-    
-            let path_dto = path_dto_option.unwrap();
-    
-            let buffer = read_file(&path_dto.old_path);
-    
-            let tree = build_tree(&language, &buffer);
-            let root_node = tree.root_node();
-            let bytes = buffer.as_ref();
-    
-            {
-                let page_file_text = build_page_file_text(&language, &root_node, bytes);
-    
-                let mut file = File::create(&path_dto.page_output_path).unwrap();
-    
-                file.write_all(page_file_text).unwrap();
-    
-                let update = object! {
-                    k: 4,
-                    p: path_dto.new_page_path,
-                    o: path_dto.page_output_path,
-                    c: "nextjs"
-                };
-    
-                println!("{}", json::stringify(update));
-            }
-    
-            let head_file_text_option = build_head_file_text(&language, &root_node, bytes);
-    
-            if let Some(head_file_text) = head_file_text_option {
-                let mut file = File::create(&path_dto.head_output_path).unwrap();
-    
-                file.write_all(head_file_text.as_bytes()).unwrap();
-    
-                let create_message = object! {
-                    k: 4,
-                    p: path_dto.new_head_path,
-                    o: path_dto.head_output_path,
-                    c: "nextjs"
-                };
-    
-                println!("{}", json::stringify(create_message));
-            }
+            build_page_directory_messages(
+                &output_directory_path,
+                &language,
+                &old_path_buf,
+            );
         });
     }
 
@@ -178,4 +135,3 @@ fn main() {
 
     println!("{}", json::stringify(finish_message));
 }
-
--- src/page_file.rs
+++ src/page_file.rs
@@ -1,4 +1,9 @@
+use std::{path::PathBuf, fs::File, io::Write};
+
+use json::object;
 use tree_sitter::{Language, Node};
+
+use crate::{paths::build_path_dto, read_file, tree::build_tree, head_file::build_head_file_text};
 
 pub fn build_page_file_text<'bytes>(
     language: &Language,
@@ -6,4 +11,58 @@ pub fn build_page_file_text<'bytes>(
     bytes: &'bytes [u8],
 ) -> &'bytes [u8] {
     bytes
+}
+
+pub fn build_page_directory_messages(
+    output_directory_path: &String,
+    language: &Language,
+    old_path_buf: &PathBuf,
+) {
+    let path_dto_option = build_path_dto(&output_directory_path, &old_path_buf);
+
+    if path_dto_option.is_none() {
+        return;
+    }
+
+    let path_dto = path_dto_option.unwrap();
+
+    let buffer = read_file(&path_dto.old_path);
+
+    let tree = build_tree(&language, &buffer);
+    let root_node = tree.root_node();
+    let bytes = buffer.as_ref();
+
+    {
+        let page_file_text = build_page_file_text(&language, &root_node, bytes);
+
+        let mut file = File::create(&path_dto.page_output_path).unwrap();
+
+        file.write_all(page_file_text).unwrap();
+
+        let update = object! {
+            k: 4,
+            p: path_dto.new_page_path,
+            o: path_dto.page_output_path,
+            c: "nextjs"
+        };
+
+        println!("{}", json::stringify(update));
+    }
+
+    let head_file_text_option = build_head_file_text(&language, &root_node, bytes);
+
+    if let Some(head_file_text) = head_file_text_option {
+        let mut file = File::create(&path_dto.head_output_path).unwrap();
+
+        file.write_all(head_file_text.as_bytes()).unwrap();
+
+        let create_message = object! {
+            k: 4,
+            p: path_dto.new_head_path,
+            o: path_dto.head_output_path,
+            c: "nextjs"
+        };
+
+        println!("{}", json::stringify(create_message));
+    }
 }
