723b897651ce49880a935ec3e1550725decfe8ad
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2022-12-13 10:05:14 +0100

    restore the test.sh file

--- test.bash
+++ test.bash
@@ -1,7 +1,5 @@
 ./target/release/nora-rust-engine \
---left "/gppd/intuita/terraform-website/components/io-card-container/index.tsx" \
---right "/home/gpp/.config/Code - Insiders/User/workspaceStorage/c48f65c978f04046d5ad3c1161c87a2b/Intuita.intuita-vscode-extension/noraNodeEngineOutput/oCqnpaZ5iwrlefhlI_ZrFQ.txt"
-# --directory "/gppd/intuita/terraform-website" \
-# --pattern "**/pages/**/*.{ts,tsx}" \
-# --antipatterns "**/node_modules/*/**" \
-# --output-directory-path "/gppd/intuita/test"
+--directory "/gppd/intuita/terraform-website" \
+--pattern "**/pages/**/*.{ts,tsx}" \
+--antipatterns "**/node_modules/*/**" \
+--output-directory-path "/gppd/intuita/test"
