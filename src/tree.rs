be887dbd8aac6468b2186441b0810e765d58773f
Greg Pabian <35925521+grzpab@users.noreply.github.com>
2023-07-05 09:38:03 +0200

    INT-1313 Allow Piranha to run from within the Codemod Engine Rust

--- src/tree.rs
+++
@@ -1,9 +0,0 @@
-use tree_sitter::{Language, Parser, Tree};
-
-pub fn build_tree(language: &Language, text: &impl AsRef<[u8]>) -> Tree {
-    let mut parser = Parser::new();
-
-    parser.set_language(*language).unwrap();
-
-    return parser.parse(text, None).unwrap();
-}
