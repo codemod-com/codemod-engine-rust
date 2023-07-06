6f2f3e1db160fa54d929b04ca2b4c6359bba9e4b
Alex Bit <78109534+alex-from-intuita@users.noreply.github.com>
2023-07-06 14:27:17 -0700

    more

---
+++ examples/index copy 4.ts
@@ -0,0 +1,15 @@
+const x = () => {
+  for (let i = 0; i < 10; i++) {
+    const x = 1;
+  }
+};
+
+for (let i = 0; i < 10; i++) {
+  const x = 1;
+}
+
+while (true) {
+  for (let i = 0; i < 10; i++) {
+    const x = 1;
+  }
+}
